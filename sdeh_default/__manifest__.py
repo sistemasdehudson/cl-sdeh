76e8d6f79031ed7b25c2e17fe376868d666e470e
concagabriel <58643247+concagabriel@users.noreply.github.com>
2023-02-08 20:20:17 -0300

    Update __manifest__.py

--- sdeh_default/__manifest__.py
+++ sdeh_default/__manifest__.py
@@ -81,6 +81,8 @@
         'https://github.com/OCA/e-commerce -b 13.0',
         'https://github.com/OCA/hr-attendance.git  -b 13.0',
         'https://github.com/OCA/social.git -b 13.0',
+        'https://github.com/OCA/helpdesk -b 13.0',
+        'https://github.com/OCA/management-system -b 13.0',
     ],
 
     'docker-images': [
